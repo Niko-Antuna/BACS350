502715a664c07ce9a403b8a1691aeddeebdeeaac
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-09-04 01:07:04 -0600

    Start over

--- week2/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from hero.views import HulkView, IronManView, BlackWidowView
-
-urlpatterns = [
-    path('hulk', HulkView.as_view()),
-    path('ironman', IronManView.as_view()),
-    path('blackwidow', BlackWidowView.as_view()),
-
-]
