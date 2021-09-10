3bb89fb55a5a045d2ac7e31ff11dcf265dd5ef9f
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-09-10 15:20:05 -0600

    Home

--- week3/Superhero/config/urls.py
+++ week3/Superhero/config/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('hulk', HulkView.as_view()),
     path('ironman', IronManView.as_view()),
     path('blackwidow', BlackWidowView.as_view()),
-    path('home', HomeView.as_view())
+    path('', HomeView.as_view())
 ]
