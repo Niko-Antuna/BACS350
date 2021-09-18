9d6aab0f803be56d12741b0131ebdffa32cc6c0d
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-09-18 17:46:51 -0600

    Done

--- week4/Superhero/config/settings.py
+++ week4/Superhero/config/settings.py
@@ -50,10 +50,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-AUTHENTICATION_BACKENDS = (
-    ('django.contrib.auth.backends.ModelBackend'),
-)
-
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
