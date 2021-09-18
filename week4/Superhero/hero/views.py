db265fd17c866a22b22d309538869d5ffe37585c
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-09-17 21:35:43 -0600

    Fresh

--- week4/Superhero/hero/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView, TemplateView
-from .models import Hero
-
-class HeroListView(ListView):
-    model = Hero
-    template_name = 'hero_list.html'
-
-class IndexView(TemplateView):
-    template_name = 'index.html'
