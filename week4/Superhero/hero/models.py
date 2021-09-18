f8eea1a84024a4e6dff701acb152dc05ccdbb941
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-09-17 22:03:32 -0600

    Part 1

--- week4/Superhero/hero/models.py
+++ week4/Superhero/hero/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-class Hero (models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.CharField(max_length=200)
+# Create your models here.
