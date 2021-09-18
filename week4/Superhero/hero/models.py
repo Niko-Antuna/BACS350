e4b7f414ccf42b379c98d821fa1bec38aa85cb38
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-09-18 00:18:23 -0600

    models

--- week4/Superhero/hero/models.py
+++ week4/Superhero/hero/models.py
@@ -4,3 +4,6 @@ class Hero (models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return super().__str__(f'Name {self.name}')
