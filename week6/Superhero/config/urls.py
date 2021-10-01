83a467d13c784bc4e90923300947189ab249ac95
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-01 16:56:28 -0600

    week6

---
+++ week6/Superhero/config/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from hero.views import IndexView, HeroDetailView, HeroListView
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view()),
+    path('hero/', HeroListView.as_view()),
+    path('hero/<int:pk>', HeroDetailView.as_view()),
+]
