252529910a7b0230c1f1d9c9caedcac3c3e20e54
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-11 14:04:05 -0600

    Static Root

--- week7/Superhero/config/settings.py
+++ week7/Superhero/config/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [join(BASE_DIR, "static"), ]
+STATIC_ROOT = "/Users/nikoantuna/Documents/Intermediate Web Dev/BACS350/week7/Superhero/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
