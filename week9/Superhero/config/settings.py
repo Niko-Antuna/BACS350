fe2b35d26089bc175feb81056a8c1f392bc218fb
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-26 22:40:44 -0600

    Project 9 Done

--- week9/Superhero/config/settings.py
+++ week9/Superhero/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hero', 'accounts'
+    'hero', 'accounts', 'doc', 'table', 'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- week9/Superhero/hero/models.py
+++ week9/Superhero/hero/models.py
@@ -15,3 +15,25 @@ class Hero (models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('hero_list')
+
+
+class Chapter(models.Model):
+    hero = models.CharField(max_length=200, default="Leverage Principle")
+    # book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    title = models.CharField(max_length=200)
+    markdown = models.TextField()
+    html = models.TextField()
+    document = models.CharField(max_length=200)
+
+    def export_record(self):
+        return [self.hero, self.order, self.title]
+
+    @staticmethod
+    def import_record(values):
+        c = Chapter.objects.get_or_create(hero=values[0], order=values[1])[0]
+        c.title = values[2]
+        c.save()
+
+    def __str__(self):
+        return f'{self.hero.name} - {self.order} - {self.title}'
