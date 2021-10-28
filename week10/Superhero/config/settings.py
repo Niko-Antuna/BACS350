41d8e02ce2495f2a6739a0a13486d6983d0e4534
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-28 13:57:37 -0600

    table removed

--- week10/Superhero/config/settings.py
+++ week10/Superhero/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hero', 'accounts', 'doc', 'table', 'crispy_forms',
+    'hero', 'accounts', 'doc', 'crispy_forms',
 ]
 
 MIDDLEWARE = [
