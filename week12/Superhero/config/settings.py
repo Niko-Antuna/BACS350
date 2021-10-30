d3f9d05de323b313d3a5bd637eb645823fb9fbba
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-29 23:03:10 -0600

    a

--- week12/Superhero/config/settings.py
+++ week12/Superhero/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hero', 'accounts'
+    'hero', 'accounts', 'crispy_forms',
 ]
 
 MIDDLEWARE = [
