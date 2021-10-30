3519d5e5347df84050ed5a321ab10e5509caf886
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2021-10-29 20:58:49 -0600

    a

--- week12/Superhero/hero/models.py
+++ week12/Superhero/hero/models.py
@@ -15,3 +15,25 @@ class Hero (models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('hero_list')
+
+
+class Chapter(models.Model):
+    book = models.CharField(max_length=200, default="Default")
+    # book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    title = models.CharField(max_length=200)
+    markdown = models.TextField()
+    html = models.TextField()
+    document = models.CharField(max_length=200)
+
+    def export_record(self):
+        return [self.book, self.order, self.title]
+
+    @staticmethod
+    def import_record(values):
+        c = Chapter.objects.get_or_create(book=values[0], order=values[1])[0]
+        c.title = values[2]
+        c.save()
+
+    def __str__(self):
+        return f'{self.book.title} - {self.order} - {self.title}'
--- week12/Superhero/hero/views_hero.py
+++ week12/Superhero/hero/views_hero.py
@@ -5,6 +5,10 @@ from django.views.generic import CreateView, DeleteView, DetailView, ListView, R
 from .models import Hero
 
 
+class HeroView(RedirectView):
+    url = '/hero/'
+
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
